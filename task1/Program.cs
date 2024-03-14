a7e851032ea344b184dcb10f1ef43a557aee504a
Timur Yumaguen <timur_red82@mail.ru>
2024-03-14 19:12:52 +0300

    задача по рекурсии 1

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в 
+//  промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+Console.WriteLine("Введите начальное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+        
+    RecursiveNumbers(m,n);
+
+    static void RecursiveNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.Write(m + " ");
+            RecursiveNumbers(m + 1, n);
+        }
+        Console.WriteLine();
+    }
+
+
+
+
+    
+
+
