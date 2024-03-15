a7ed00394cf43e1a2de1e379560d3be14266a3a7
Timur Yumaguen <timur_red82@mail.ru>
2024-03-15 12:36:02 +0300

    решение задачи на разворот массива с помощью рекурсии

---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+int[] arr = new int[5];
+for (int i = 0; i < arr.Length; i++)
+{
+    Random rnd = new Random();
+    arr[i] = rnd.Next(10);
+}
+void PrintArr(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+static void PrintReversArr(int[] arr, int index)
+{
+    if (index < 0)
+        return;
+    Console.Write(arr[index] + " ");
+    PrintReversArr(arr, index - 1);
+}
+PrintArr(arr);
+PrintReversArr(arr, 4);
+
